d174155bc0f6d6575c321ba3e4c558e45732b62a
duchau <haudd@bsscommerce.com>
2024-11-12 09:15:10 +0700

    edit ui post

--- components/posts/social-post.tsx
+++ components/posts/social-post.tsx
@@ -25,7 +25,7 @@ export default function Post() {
 
   return (
     <PostDialog open={isPostOpen} setOpen={setIsPostOpen}>
-      <Card className="md:w-[580px] bg-background">
+      <Card className="md:w-[580px] bg-sidebar">
         <CardHeader className="flex flex-row items-center space-x-4 p-4">
           <Avatar className="w-10 h-10">
             <AvatarImage src="https://images.unsplash.com/photo-1457369804613-52c61a468e7d?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
