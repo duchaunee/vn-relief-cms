25b876da6a184a6fca5e0c7a7ccc3d76f8cf240a
duchau <haudd@bsscommerce.com>
2024-12-05 15:24:50 +0700

    update ui chi-tiet-cuu-tro

--- app/(main-layout)/cuu-tro-khan-cap/_components/middle-page.tsx
+++ app/(main-layout)/cuu-tro-khan-cap/_components/middle-page.tsx
@@ -4,21 +4,37 @@ import { ImagePlus } from "lucide-react";
 
 const Middle = () => {
   return (
-    <Card className="rounded-sm self-start">
-      <CardHeader className="pb-3">
-        <h2 className="text-lg font-medium">
-          Tổng hợp hình ảnh tình hình thực tế
-        </h2>
-      </CardHeader>
-      <CardContent>
-        <div className="border-2 border-dashed rounded-lg h-[400px] flex items-center justify-center">
-          <Button variant="ghost" className="flex flex-col gap-2">
-            <ImagePlus className="w-8 h-8 text-muted-foreground" />
-            <span className="text-sm text-muted-foreground">Thêm ảnh</span>
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+    <div className="flex-1 flex flex-col gap-4">
+      <Card className="w-full rounded-sm">
+        <CardHeader className="pb-3">
+          <h2 className="text-lg font-medium">
+            Các đoàn hỗ trợ cho trường hợp này
+          </h2>
+        </CardHeader>
+        <CardContent>
+          <div className="border-2 border-dashed rounded-lg py-10 flex items-center justify-center">
+            <span>Chưa có đoàn nào</span>
+          </div>
+        </CardContent>
+      </Card>
+      <Card className="w-full rounded-sm">
+        <CardHeader className="pb-3">
+          <h2 className="text-lg font-medium">
+            Tổng hợp hình ảnh tình hình thực tế
+          </h2>
+        </CardHeader>
+        <CardContent>
+          <button className="border-2 border-dashed border-gray-300 rounded-lg h-[150px] aspect-square flex items-center justify-center lg:hover:border-gray-500">
+            <div className="flex flex-col items-center gap-2 p-2">
+              <ImagePlus className="w-5 h-5 text-muted-foreground" />
+              <span className="text-sm text-muted-foreground font-medium">
+                Thêm ảnh
+              </span>
+            </div>
+          </button>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
