80404a58d254b3e4b23ec0d97b45bfae8a5c2763
duchau <haudd@bsscommerce.com>
2024-12-12 02:30:41 +0700

    fix: fix yarn-build

--- app/(main-layout)/tai-khoan/_components/list-content.tsx
+++ app/(main-layout)/tai-khoan/_components/list-content.tsx
@@ -1,14 +1,11 @@
 import { TabsContent } from "@radix-ui/react-tabs";
-import React from "react";
-import TabAccount from "./tab-content/tab-account";
-import TabRequest from "./tab-content/tab-request";
 import { menuAccountItems } from "@/constants/info-account";
 
 const ListContent = () => {
   return (
     <div className="settings-container">
       {menuAccountItems.map(({ value, component: TabComponent }) => (
-        <TabsContent value={value}>
+        <TabsContent key={value} value={value}>
           <TabComponent />
         </TabsContent>
       ))}
--- app/(main-layout)/tai-khoan/_components/nav-bar/nav-bar.laptop.tsx
+++ app/(main-layout)/tai-khoan/_components/nav-bar/nav-bar.laptop.tsx
@@ -1,12 +1,12 @@
 import { TabsTrigger } from "@/components/ui/tabs";
 import { menuAccountItems } from "@/constants/info-account";
-import { CircleUser, CreditCard, PaintBucket, Bell, Eye } from "lucide-react";
 
 const NavbarLaptop = () => {
   return (
     <div className="tabs-trigger-mobile hidden lg:flex flex-col items-center gap-1">
       {menuAccountItems.map(({ value, label, icon: Icon }) => (
         <TabsTrigger
+          key={value}
           value={value}
           className="flex items-center gap-2 justify-start w-full py-2.5 px-3 hover:bg-gray-100 data-[state=active]:shadow-none text-[#111820] data-[state=active]:bg-blue-50 data-[state=active]:text-blue-600"
         >
